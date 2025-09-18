772c8fcff5ac83c849293841c08b429047d587ad
sindri-afk <sindribjarka@gmail.com>
2025-09-18 11:33:32 +0000

    .

---
+++ vika5/puzzlesolver.cpp
@@ -0,0 +1,14 @@
+// secret, evil bit, checksum, knock i einhverri roð?
+//
+//
+
+int main(int argc, char* argv[]) {
+    // function to call
+
+    // if Greetings from S.E.C.R.E.T. then call secret function
+
+}
+
+// secret function
+
+// etc
