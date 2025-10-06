c6ed76da3b5d38c10d9c610b277240d6e3641d3b
sindri-afk <sindribjarka@gmail.com>
2025-10-06 14:56:59 +0000

    project structure set up

---
+++ vika8/tsamgroup21.cpp
@@ -0,0 +1,10 @@
+/*
+Assignment Description: 
+Do not hardcode the ip address or port numbers in your code.
+*/
+
+int main(int argc, char* argv[]) {
+    std::string groupName = "tsamgroup21"; 
+    return 0; 
+}
+
