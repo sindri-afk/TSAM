233918a8b44efd5e77ff18d9d1d0b12e45919086
sindri-afk <sindribjarka@gmail.com>
2025-10-07 17:49:51 +0000

    setups

---
+++ vika8/clientx.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+void listenServer() {
+    // Implementation for listening server
+    std::cout << "listening to server ..." << std::endl; 
+}
+
+int main() {
+    return 0; 
+}
--- vika8/tsamgroup21.cpp
+++ vika8/tsamgroup21.cpp
@@ -3,8 +3,21 @@ Assignment Description:
 Do not hardcode the ip address or port numbers in your code.
 */
 
+#include <iostream>
+#include <string>
+
+// class Client {}
+
+int openSocket() { return 0; }
+
+void closeSocket() {}
+
+void clientCommand() {}
+
 int main(int argc, char* argv[]) {
     std::string groupName = "tsamgroup21"; 
+
+    // here we need to create a TCP server that listens on a specific port
     return 0; 
 }
 
